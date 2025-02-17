d9b150696afed57291eb0034d60823072e539769
gerni2015 <gtersa.lestonnac@gmail.com>
2025-02-17 19:30:36 +0100

    Base del projecte

--- DjangoProjectWeb/settings.py
+++ DjangoProjectWeb/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'WebProjecte/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
