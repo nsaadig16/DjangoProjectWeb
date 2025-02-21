39582b5c37c95d56fca3d3062f8b2e35b886ae86
gerni2015 <gtersa.lestonnac@gmail.com>
2025-02-21 14:21:43 +0100

    Disseny

--- WebProjecte/forms.py
+++ WebProjecte/forms.py
@@ -22,3 +22,10 @@ class CustomUserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Las contraseñas no coinciden.")
         return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)  # No guardamos todavía en la BD
+        user.set_password(self.cleaned_data["password1"])  # Encriptamos la contraseña
+        if commit:
+            user.save()
+        return user
