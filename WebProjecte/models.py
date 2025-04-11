7386e535cfa99ce31fab808e34beaaf3b6856884
pllinasv <pol.llinas06@gmail.com>
2025-04-11 16:20:31 +0200

    feat(models.py): Deleted previous models, deleted previous bbdd

--- WebProjecte/models.py
+++ WebProjecte/models.py
@@ -5,14 +5,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.email
-
 class Collection(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
@@ -60,17 +52,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    try:
-        instance.profile.save()
-    except Profile.DoesNotExist:
-        Profile.objects.create(user=instance)
