c13d77f2eab81f1f460efd54e1d49da5f6ca6cd6
gerni2015 <gtersa.lestonnac@gmail.com>
2025-05-13 10:58:49 +0200

    Bug fixed (update image)

--- WebProjecte/apps.py
+++ WebProjecte/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
-class WebprojecteConfig(AppConfig):
+class WebProjecteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'WebProjecte'
+
+    def ready(self):
+        import WebProjecte.signals
