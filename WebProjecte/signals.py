b85119112a7ebf8f2bf743897d5344953efddf36
X50-45A <93247262+X50-45A@users.noreply.github.com>
2025-05-23 19:42:39 +0200

    Els tests funcionen perfectament i ara si que es eliminen les imatges creades.

--- WebProjecte/signals.py
+++ WebProjecte/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
-import os
 
 @receiver(pre_save, sender=Profile)
 def delete_old_profile_image(sender, instance, **kwargs):
@@ -17,10 +16,16 @@ def delete_old_profile_image(sender, instance, **kwargs):
     new_image = instance.profile_image
 
     if old_image and old_image != new_image:
-        old_image.delete(save=False)
+        try:
+            old_image.storage.delete(old_image.name)
+        except Exception:
+            # Opcional: loguear error o pasar
+            pass
 
 @receiver(post_delete, sender=Profile)
 def delete_profile_image_file(sender, instance, **kwargs):
-    if instance.profile_image and instance.profile_image.path:
-        if os.path.isfile(instance.profile_image.path):
-            os.remove(instance.profile_image.path)
+    if instance.profile_image:
+        try:
+            instance.profile_image.storage.delete(instance.profile_image.name)
+        except Exception:
+            pass
